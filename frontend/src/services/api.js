fe4febf62d32fe07bd5613a7a4843e9e23e68d9b
spide15 <gadadeep4@gmail.com>
2025-06-26 15:32:54 +0530

    apiurlll

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-// const API_URL = 'https://warehouse-supply-chain-system.onrender.com/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://warehouse-supply-chain-system.onrender.com/api';
 
 export const login = (email, password) =>
   axios.post(`${API_URL}/auth/login`, { email, password });
